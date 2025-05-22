2696c37c615109acedc4541b7ffb353de47c2d11
vky <nxtwave@ip-10-2-165-9.ap-south-1.compute.internal>
2025-05-22 15:07:24 +0000

    removed the lazy loading concept

--- src/App.js
+++ src/App.js
@@ -1,95 +1,55 @@
-import React, {useState, useEffect, useCallback} from 'react'
-import {Switch, Route, useLocation} from 'react-router-dom'
-import Loader from 'react-loader-spinner'
+import React from 'react'
+import {Switch, Route} from 'react-router-dom'
 import './App.css'
+
+import PopularMovies from './components/PopularMovies'
+import TopRatedMovies from './components/TopRatedMovies'
+import UpcomingMovies from './components/UpcomingMovies'
+import SingleMovieDetails from './components/SingleMovieDetails'
+import SearchedMovies from './components/SearchedMovies'
 
 const routeConfigs = [
   {
     path: '/',
     key: 'PopularMovies',
     exact: true,
-    test: path => path === '/',
-    importPath: './components/PopularMovies',
+    component: PopularMovies,
   },
   {
     path: '/top-rated',
     key: 'TopRatedMovies',
     exact: true,
-    test: path => path === '/top-rated',
-    importPath: './components/TopRatedMovies',
+    component: TopRatedMovies,
   },
   {
     path: '/upcoming',
     key: 'UpcomingMovies',
     exact: true,
-    test: path => path === '/upcoming',
-    importPath: './components/UpcomingMovies',
+    component: UpcomingMovies,
   },
   {
     path: '/movies/:id',
     key: 'SingleMovieDetails',
     exact: true,
-    test: path => /^\/movies\/[^/]+$/.test(path),
-    importPath: './components/SingleMovieDetails',
+    component: SingleMovieDetails,
   },
   {
     path: '/movies/search/:search',
     key: 'SearchedMovies',
     exact: true,
-    test: path => path.startsWith('/movies/search/'),
-    importPath: './components/SearchedMovies',
+    component: SearchedMovies,
   },
 ]
 
 const App = () => {
-  const location = useLocation()
-  const [components, setComponents] = useState({
-    PopularMovies: null,
-    TopRatedMovies: null,
-    UpcomingMovies: null,
-    SingleMovieDetails: null,
-    SearchedMovies: null,
-  })
-
-  const loadComponent = useCallback(
-    async path => {
-      const match = routeConfigs.find(
-        ({key, test}) => test(path) && !components[key],
-      )
-      if (match) {
-        try {
-          const mod = await import(`${match.importPath}`)
-          setComponents(prev => ({...prev, [match.key]: mod.default}))
-        } catch (error) {
-          console.error(`Failed to load ${match.key}:`, error)
-        }
-      }
-    },
-    [components],
-  )
-
-  useEffect(() => {
-    loadComponent(location.pathname)
-  }, [location.pathname, loadComponent])
-
-  const renderLoader = () => (
-    <div className="loader-style">
-      <Loader type="ThreeDots" color="#3b82f6" height={80} width={80} />
-    </div>
-  )
-
   return (
     <Switch>
-      {routeConfigs.map(({path, key, exact}) => (
+      {routeConfigs.map(({path, key, exact, component: Component}) => (
         <Route
-          key={path}
+          key={key}
           path={path}
           exact={exact}
-          render={props =>
-            components[key]
-              ? React.createElement(components[key], props)
-              : renderLoader()
-          }
+          render={props => <Component {...props} />}
         />
       ))}
     </Switch>
